0193c935fe1052c35c6c523b8add375357b34c89
Darko-Martinovic <darko.martinovic@outlook.com>
2018-07-20 10:22:07 +0200

    Small changes in code

--- TestWordAddIn/MyRibbon.cs
+++ TestWordAddIn/MyRibbon.cs
@@ -1,27 +1,12 @@
 ﻿using System;
+using System.Drawing;
 using System.IO;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Windows.Forms;
 using Office = Microsoft.Office.Core;
 using Word = Microsoft.Office.Interop.Word;
 
-// TODO:  Follow these steps to enable the Ribbon (XML) item:
-
-// 1: Copy the following code block into the ThisAddin, ThisWorkbook, or ThisDocument class.
-
-//  protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
-//  {
-//      return new MyRibbon();
-//  }
-
-// 2. Create callback methods in the "Ribbon Callbacks" region of this class to handle user
-//    actions, such as clicking a button. Note: if you have exported this Ribbon from the Ribbon designer,
-//    move your code from the event handlers to the callback methods and modify the code to work with the
-//    Ribbon extensibility (RibbonX) programming model.
-
-// 3. Assign attributes to the control tags in the Ribbon XML file to identify the appropriate callback methods in your code.  
-
-// For more information, see the Ribbon XML documentation in the Visual Studio Tools for Office Help.
 
 
 namespace TestWordAddIn
@@ -29,18 +14,42 @@ namespace TestWordAddIn
     [ComVisible(true)]
     public class MyRibbon : Office.IRibbonExtensibility
     {
-        public void OnTextButton(Office.IRibbonControl control)
+
+
+        /// <summary>
+        /// Test spelling
+        /// </summary>
+        /// <param name="control"></param>
+        public void OnSpellingButton(Office.IRibbonControl control)
         {
-            var currentRange = Globals.ThisAddIn.Application.Selection.Range;
-            currentRange.Text = "This text was added by the Ribbon.";
+            object startLocation = Globals.ThisAddIn.Application.ActiveDocument.Content.Start;
+            object endLocation = Globals.ThisAddIn.Application.ActiveDocument.Content.End;
+
+            var spellCheck = Globals.ThisAddIn.Application.CheckSpelling(Globals.ThisAddIn.Application.ActiveDocument
+                .Range(ref startLocation, ref endLocation).Text);
+
+            MessageBox.Show(spellCheck ? @"Everything OK" : @"There are mistakes");
+        }
+
+        public void OnNumberOfWords(Office.IRibbonControl control)
+        {
+
+            var rng = Globals.ThisAddIn.Application.ActiveDocument.Content;
+            rng.Select();
+
+
+            MessageBox.Show($@"Characters including hidden in document : {Globals.ThisAddIn.Application.ActiveDocument.Characters.Count}",@"Info",MessageBoxButtons.OK,MessageBoxIcon.Information);
+
+
+
         }
         public void OnTableButton(Office.IRibbonControl control)
         {
-            object missing = Type.Missing;
-            Word.Range currentRange = Globals.ThisAddIn.Application.Selection.Range;
-            Word.Table newTable = Globals.ThisAddIn.Application.ActiveDocument.Tables.Add(currentRange, 3, 4, ref missing, ref missing);
+            var missing = Type.Missing;
+            var currentRange = Globals.ThisAddIn.Application.Selection.Range;
+            var newTable = Globals.ThisAddIn.Application.ActiveDocument.Tables.Add(currentRange, 3, 4, ref missing, ref missing);
             // Get all of the borders except for the diagonal borders.
-            Word.Border[] borders = new Word.Border[6];
+            var borders = new Word.Border[6];
             borders[0] = newTable.Borders[Word.WdBorderType.wdBorderLeft];
             borders[1] = newTable.Borders[Word.WdBorderType.wdBorderRight];
             borders[2] = newTable.Borders[Word.WdBorderType.wdBorderTop];
@@ -48,7 +57,7 @@ namespace TestWordAddIn
             borders[4] = newTable.Borders[Word.WdBorderType.wdBorderHorizontal];
             borders[5] = newTable.Borders[Word.WdBorderType.wdBorderVertical];
             // Format each of the borders.
-            foreach (Word.Border border in borders)
+            foreach (var border in borders)
             {
                 border.LineStyle = Word.WdLineStyle.wdLineStyleSingle;
                 border.Color = Word.WdColor.wdColorBlue;
@@ -56,10 +65,23 @@ namespace TestWordAddIn
 
         }
 
+        public Bitmap spell_get(Office.IRibbonControl control)
+        {
+            return Properties.Resources.spell1;
+        }
 
-            #region IRibbonExtensibility Members
+        public Bitmap word_get(Office.IRibbonControl control)
+        {
+            return Properties.Resources.word;
+        }
+        public Bitmap table_get(Office.IRibbonControl control)
+        {
+            return Properties.Resources.table;
+        }
 
-            public string GetCustomUI(string ribbonId)
+        #region IRibbonExtensibility Members
+
+        public string GetCustomUI(string ribbonId)
         {
             return GetResourceText("TestWordAddIn.MyRibbon.xml");
         }
@@ -71,6 +93,7 @@ namespace TestWordAddIn
 
         public void Ribbon_Load(Office.IRibbonUI ribbonUi)
         {
+
         }
 
         #endregion
--- TestWordAddIn/Properties/Resources.Designer.cs
+++ TestWordAddIn/Properties/Resources.Designer.cs
@@ -9,6 +9,7 @@
 //------------------------------------------------------------------------------
 
 namespace TestWordAddIn.Properties {
+    using System;
     
     
     /// <summary>
@@ -18,10 +19,10 @@ namespace TestWordAddIn.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -35,7 +36,7 @@ namespace TestWordAddIn.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TestWordAddIn.Properties.Resources", typeof(Resources).Assembly);
@@ -50,12 +51,52 @@ namespace TestWordAddIn.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        public static System.Drawing.Icon spell {
+            get {
+                object obj = ResourceManager.GetObject("spell", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap spell1 {
+            get {
+                object obj = ResourceManager.GetObject("spell1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap table {
+            get {
+                object obj = ResourceManager.GetObject("table", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap word {
+            get {
+                object obj = ResourceManager.GetObject("word", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
